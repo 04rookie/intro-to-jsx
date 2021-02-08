916fe784144c9b691cca9306ed59e703528288a6
04rookie <47923475+04rookie@users.noreply.github.com>
2021-02-08 13:13:42 +0530

    used {} to use JS inside tag

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+var favItem = "Naan";
+
 ReactDOM.render(
   <div>
     <h1>My favourite food</h1>
     <ul>
       <li>Jamun</li>
       <li>Biryani</li>
-      <li>pizza</li>
+      <li>Pizza</li>
+      <li>{favItem}</li>
     </ul>
   </div>,
   document.getElementById("root")
