8774ef4e1c8e6a7195ebe3524428ffe9c7b1d322
04rookie <47923475+04rookie@users.noreply.github.com>
2021-02-08 12:21:46 +0530

    added some html Basic header and a list of items

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favourite food</h1>
+    <ul>
+      <li>Jamun</li>
+      <li>Biryani</li>
+      <li>pizza</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
